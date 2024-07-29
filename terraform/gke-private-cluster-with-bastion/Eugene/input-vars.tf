8f059875a40422e2b6e138c06e2ad72437e28185
unknown <eugeneanimante2@gmail.com>
2024-07-29 00:32:00 -0400

    Fixed strange issue with variables

--- terraform/gke-private-cluster-with-bastion/Eugene/input-vars.tf
+++ terraform/gke-private-cluster-with-bastion/Eugene/input-vars.tf
@@ -1,9 +1,11 @@
 variable "project_id" {
   description = "GCP project id"
+  default = "cn8001-iac-kubernetes-terra"
 }
 
 variable "region" {
   description = "GCP project location"
+  default = "us-central1"
 }
 
 #variable "cluster_subnets" {
@@ -19,9 +21,25 @@ variable "region" {
 variable "bastion_startup_script" {
   description = "Startup script for bastion host (jumpbox)"
   type        = string
+  default = <<-EOT
+  #!/bin/bash
+  sudo apt-get update
+  sudo apt-get install -yq git
+EOT
 }
 
 variable "service_account_roles" {
   description = "List of roles to be assigned to the bastion service account"
   type        = list(string)
+  default = [
+  "roles/logging.logWriter",
+  "roles/monitoring.metricWriter",
+  "roles/monitoring.viewer",
+  "roles/compute.osLogin",
+  "roles/compute.admin",
+  "roles/iam.serviceAccountUser",
+  "roles/container.admin",
+  "roles/container.clusterAdmin",
+  "roles/compute.osAdminLogin"
+]
 }
